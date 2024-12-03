9636a358abf05d0477042a6e3c7442aa230f3201
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2024-12-02 23:20:55 -0300

    feat: add limit spaceship move and create shots

--- scripts/game.js
+++ scripts/game.js
@@ -6,11 +6,19 @@ const score = document.querySelector(".score");
 const gameOverButton = document.querySelector(".gameOver button");
 
 const spaceContainerWidth = spaceContainer.offsetWidth;
-const spaceContainerHeigth = spaceContainer.offsetHeigth;
+const spaceContainerHeigth = spaceContainer.offsetHeight;
+
 const spaceshipWidth = spaceShip.offsetWidth;
-const spaceshipHeigth = spaceShip.offsetHeigth;
+const spaceshipHeigth = spaceShip.offsetHeight;
 
 const spaceshipSpeed = 10; // px to upper
+const shootSpeed = 10; // per second
+const spaceshipDamage = 25; // -25 per shot
+const timeToEndSpecialShot = 30 * 1000;
+
+let canShoot = true;
+let specialShotIsActive = false;
+let shoot = 25; // -25 enemy life
 
 let positionX = 0;
 let positionY = 0;
@@ -21,15 +29,64 @@ function spaceshipeMove() {
   moveX += positionX * spaceshipSpeed;
   moveY += positionY * spaceshipSpeed;
 
-  spaceShip.style.left = moveX + "px";
-  spaceShip.style.bottom = moveY + "px";
+  const discountScreenLimit = spaceshipWidth / 2;
+
+  moveX = Math.max(
+    discountScreenLimit,
+    Math.min(moveX, spaceContainerWidth - discountScreenLimit)
+  );
+
+  moveY = Math.max(
+    -discountScreenLimit,
+    Math.min(
+      moveY,
+      spaceContainerHeigth - spaceshipHeigth - discountScreenLimit
+    )
+  );
+
+  spaceShip.style.left = moveX - discountScreenLimit + "px";
+  spaceShip.style.bottom = moveY + discountScreenLimit + "px";
 
   requestAnimationFrame(spaceshipeMove);
+}
+
+function creatShot(className = "shot") {
+  if (canShoot) {
+    const shot = document.createElement("div");
+    shot.classList.add(className);
+
+    if (specialShotIsActive) {
+      shot.classList.add("specialShot");
+
+      const shootSound = new Audio("../audios/shootSpecial.mp3");
+      shootSound.volume = 0.3;
+      shootSound.play();
+
+      shot.style.left = moveX + "px";
+      shot.style.bottom = moveY + spaceshipHeigth + spaceshipHeigth / 8 + "px";
+    } else {
+      const shootSound = new Audio("../audios/shoot.mp3");
+      shootSound.volume = 1;
+      shootSound.play();
+
+      shot.style.left = moveX + "px";
+      shot.style.bottom = moveY + spaceshipHeigth + spaceshipHeigth / 4 + "px";
+    }
+
+    spaceContainer.appendChild(shot);
+
+    canShoot = false;
+
+    setTimeout(() => {
+      canShoot = true;
+    }, 1000 / shootSpeed);
+  }
 }
 
 function gameControls(key) {
   switch (key.code) {
     case "Space":
+      creatShot();
       break;
 
     case "ArrowUp":
@@ -57,6 +114,18 @@ function gameControls(key) {
     default:
       break;
   }
+}
+
+function spaceshipShootRemove() {
+  const shoots = document.querySelectorAll(".shot");
+
+  shoots.forEach((shot) => {
+    shot.addEventListener("animationend", () => {
+      shot.remove();
+    });
+  });
+
+  requestAnimationFrame(spaceshipShootRemove);
 }
 
 function gameControlsCancel(key) {
@@ -93,3 +162,4 @@ document.addEventListener("keyup", gameControlsCancel);
 
 setPLayerName();
 spaceshipeMove();
+spaceshipShootRemove();
